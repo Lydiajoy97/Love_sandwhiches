0bb5cbad9336f2bd19d12307a78571aafc7c6612
Lydia Young <lydsjoyy@gmail.com>
2023-07-01 07:58:16 +0000

    create update worksheet function which combines the two seperate functions

--- run.py
+++ run.py
@@ -48,23 +48,11 @@ def validate_data(values):
 
     return True
 
-def update_sales_worksheet(data):
-    """
-    Update sales worsheet
-    """
-    print("updating sales worksheet...")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("sales worksheet updated sucessfully.\n")
-
-def update_surplus_worksheet(data):
-    """
-    Update surplus worsheet
-    """
-    print("updating sales worksheet...")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("surplus worksheet updated sucessfully.\n")
+def update_worksheet(data, worksheet):
+    print(f"updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated sucessfully\n")
 
 def calculate_surplus_data(sales_row):
     """
@@ -85,9 +73,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to love sandwiches data automation")
 main()
